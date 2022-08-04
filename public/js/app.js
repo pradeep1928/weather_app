fe3dc1cc784da9df3efdce2834b6a5d92ec0fed3
pradeep <ppadmukhi28@gmail.com>
2022-08-04 11:47:30 +0530

    added humidity to forecast

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,8 @@ const search = document.querySelector('input');
 const msg_1 = document.querySelector('#msg_1')
 const msg_2 = document.querySelector('#msg_2')
 const msg_3 = document.querySelector('#msg_3')
+const msg_4 = document.querySelector('#msg_4')
+
 
 
 
@@ -15,7 +17,9 @@ weatherForm.addEventListener('submit', (e) => {
 
     msg_1.textContent = 'Loading...'
     msg_2.textContent = ''
-    msg_2.textContent = ''
+    msg_3.textContent = ''
+    msg_4.textContent = ''
+
 
     fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
@@ -26,6 +30,8 @@ weatherForm.addEventListener('submit', (e) => {
             msg_1.textContent = data.location
             msg_2.textContent = data.forecast.description
             msg_3.textContent = data.forecast.temperature
+            msg_4.textContent = data.forecast.humidity
+
 
         }
     })
